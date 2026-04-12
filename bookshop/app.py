488b44ebaffd88d3934ae87cd3765120b8a1bdd1
rodakt <tomasz.rodak@wmii.uni.lodz.pl>
2026-04-12 22:21:27 +0200

    rm /bookshop

--- bookshop/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-from views.books import books_bp
-
-# static_url_path="" maps bookshop/static/ to the root URL,
-# so static/robots.txt is served at /robots.txt
-app = Flask(__name__, static_url_path="", static_folder="static")
-app.register_blueprint(books_bp)
